fee1b6fc04a076de3f606020ceae7f2d203bc225
Pklove3459 <61339804+Pklove3459@users.noreply.github.com>
2020-11-06 13:13:09 -0600

    WCFPRPPRPR

---
+++ Duplex service WCF/Contratos/ICalculatorDuplexCallback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contratos
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples", SessionMode = SessionMode.Required,
+                 CallbackContract = typeof(ICalculatorDuplexCallback))]
+    public interface ICalculatorDuplexCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void Equals(double result);
+        [OperationContract(IsOneWay = true)]
+        void Equation(string eqn);
+    }
+}
